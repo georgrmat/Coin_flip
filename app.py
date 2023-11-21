a84246e77175376ddee51f2172618d920ef62dcc
Georges <92797658+georgrmat@users.noreply.github.com>
2023-11-21 11:35:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,28 @@
-import streamlit as st
+# https://coinflip-xv7vbpgqbrbskm3niqyurg.streamlit.app/
 import numpy as np
+import streamlit as st
 
-
-
-def main():
-    st.title("User Input Demo")
+st.title("Coin flip simulator")
     
-    # User input for N (integer)
-    n = st.number_input("Select an integer value for N", min_value=1, max_value=1000000, step=1)
+# User input for N (integer)
+N = st.number_input("Select the number of flips", min_value=1, max_value=1000000, step=1)
     
-    # User input for p (float)
-    p = st.number_input("Select a float value for p", min_value=0.0, max_value=1.0, step=0.05)
+# User input for p (float)
+p = st.number_input("Select the probability of heads", min_value=0.0, max_value=1.0, step=0.01)
     
-    # Display the selected values
-    st.write("Selected N:", n)
-    st.write("Selected p:", p)
+# Display the selected values
+st.write("Selected N:", n)
+st.write("Selected p:", p)
 
-if __name__ == "__main__":
-    main()
 
 flips = np.random.random(N)
 
 nb_heads = np.sum(np.where(flips < p, 1, 0))
 nb_tails = N - nb_heads
+st.write("Number of heads:", nb_heads)
+st.write("Number of tails:", nb_tails)
 
 percentage_heads = np.round(100*nb_heads/N, 2)
 percentage_tails = np.round(100*nb_tails/N, 2) 
-
+st.write("Percentage of heads:", percentage_heads)
+st.write("Percentage of tails:", percentage_tails)
