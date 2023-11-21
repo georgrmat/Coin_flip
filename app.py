8aa2caf2120a88644b716dcdf56e1f93ba30c5db
Georges <92797658+georgrmat@users.noreply.github.com>
2023-11-21 11:27:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 import streamlit as st
 import numpy as np
 
-N = 
-p = 
+
+
+def main():
+    st.title("User Input Demo")
+    
+    # User input for N (integer)
+    n = st.number_input("Select an integer value for N", min_value=1, max_value=1000000, step=1)
+    
+    # User input for p (float)
+    p = st.number_input("Select a float value for p", min_value=0.0, max_value=1.0, step=0.05)
+    
+    # Display the selected values
+    st.write("Selected N:", n)
+    st.write("Selected p:", p)
+
+if __name__ == "__main__":
+    main()
 
 flips = np.random.random(N)
 
